07665928e6c23731a59bdd0a17aa46998c7fcd32
Ricardo-1998 <00004817@uca.edu.sv>
2019-04-09 17:07:59 -0600

    adaptador y modelo

--- app/src/main/java/com/example/monedas/MainActivity.kt
+++ app/src/main/java/com/example/monedas/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.monedas
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
